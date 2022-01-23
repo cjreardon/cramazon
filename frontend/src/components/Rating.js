f578507a9058c0539352d9e100e147c2e8b2c133
cjreardon <connorreardon3@gmail.com>
2022-01-23 14:17:21 -0500

    added rating component

---
+++ frontend/src/components/Rating.js
@@ -0,0 +1,63 @@
+export default function Rating(props) {
+  const { rating, numreviews } = props;
+  return (
+    <div className="rating">
+      <span>
+        <i
+          className={
+            rating >= 1
+              ? "fa fa-star"
+              : rating >= 0.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            rating >= 2
+              ? "fa fa-star"
+              : rating >= 1.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            rating >= 3
+              ? "fa fa-star"
+              : rating >= 2.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            rating >= 4
+              ? "fa fa-star"
+              : rating >= 3.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            rating >= 5
+              ? "fa fa-star"
+              : rating >= 4.5
+              ? "fas fa-star-half-alt"
+              : "far fa-star"
+          }
+        ></i>
+      </span>
+      <span>{numreviews + " reviews"}</span>
+    </div>
+  );
+}
